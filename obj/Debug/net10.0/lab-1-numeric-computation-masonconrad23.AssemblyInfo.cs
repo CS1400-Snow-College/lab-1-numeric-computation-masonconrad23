d719e5c155ef40d6c9b244b57fdc245dc69dbe20
Mason Conrad <masonconrad23@gmail.com>
2026-06-29 21:15:25 -0600

    finished step: 1

--- obj/Debug/net10.0/lab-1-numeric-computation-masonconrad23.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-1-numeric-computation-masonconrad23.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-masonconrad23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03034f82b4d2637dd53e6ea49afaa00a103054c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9567c9f0a4c843873d0c083bc9d1e38d61165b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-masonconrad23")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-masonconrad23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
